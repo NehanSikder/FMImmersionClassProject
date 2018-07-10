81ab1ef7879b206245506215da90be3c7b431a14
Nehan <ans2ur@virginia.edu>
2018-07-09 22:00:48 -0400

    Refactored calculator code

--- client/proj/src/app/home/home.component.ts
+++ client/proj/src/app/home/home.component.ts
@@ -10,9 +10,9 @@ export class HomeComponent implements OnInit {
   rate: number;
   principal: number;
   years: number;
-  total: number;
-
-
+  paymentAmount: number;
+  totalInterest: number;
+  totalCost: number;
 
   constructor() { }
 
@@ -21,25 +21,59 @@ export class HomeComponent implements OnInit {
   }
 
   calculate() {
-  	this.total = this.principal+this.rate+this.years;
+  	// set value for paymentAmount
+    var fixedPMT = this.fixedPMT(this.principal, this.rate, this.years);
+    this.paymentAmount = this.calculatePrincipal(fixedPMT, this.principal, this.rate)
+    console.log('fixedPMT',fixedPMT);
+    console.log('this.principal',this.principal);
+    console.log('this.rate',this.rate);
+    console.log('this.calculatePrincipal',this.calculatePrincipal(fixedPMT, this.principal, this.rate));
+
+
+
+    // set value for paymentAmount
+
+    // set value for paymentAmount
+
   }
   
 
   testFixedPMT() {
-    testSet = [(0,0,0), (100000, 6, 15)];
+   // testSet = [(0,0,0), (100000, 6, 15)];
     // test not complete
   }
 
-  fixedPMT(principle, rate, years){
-    // P - principle
+  fixedPMT(principal, rate, years){
+    // P - principal
     // R - annual interest rate as a decimal!!!
     // Y - number of years
     var months = 12 * years;
     var monthlyRate = rate/12;
-    var temp = (monthlyRate * Math.pow((1 + monthlyRate),months))/(Math.pow((1 + monthlyRates),months) - 1);
-    return principle * temp;
+    var temp = (monthlyRate * Math.pow((1 + monthlyRate),months))/(Math.pow((1 + monthlyRate),months) - 1);
+    return principal * temp;
   }
 
+  calculatePrincipal(fixedPMT, principalInput, rate) {
+    // fixedPMT is the fixed payment 
+    // P is the current principalInput amount
+    // R is the annual interest rate
+    var monthlyRate = rate/12;
+    var next = principalInput - (principalInput* monthlyRate)
+    return fixedPMT - (principalInput - next)
+    
+  }
+
+  calculateInterest(fixedPMT, principalInput, rate) {
+    // fixedPMT is the fixed payment 
+    // P is the current principalInput amount
+    // R is the annual interest rate
+    var monthlyRate = rate / 12;
+    var next = principalInput - (principalInput * monthlyRate)
+    return (principalInput - next)
+  }
+
+
+/*
   findPaymentStatistics(principle, rate, years) {
     // returns a map of principle paid and interest paid per month
     var payments = years * 12;
@@ -57,28 +91,8 @@ export class HomeComponent implements OnInit {
       // update the leftover principle that still needs to be paid
       currentPrinciple = currentPrinciple - returnMap['principle'][i];
     }
-
-
-
   }
-  principle(fixedPMT, principle, rate) {
-    // fixedPMT is the fixed payment 
-    // P is the current principle amount
-    // R is the annual interest rate
-    var monthlyRate = rate/12;
-    var next = principle - (principle* monthlyRate)
-    return fixedPMT - (principle - next)
-    
-  }
+*/
 
-  interest(fixedPMT, principle, rate) {
-    // fixedPMT is the fixed payment 
-    // P is the current principle amount
-    // R is the annual interest rate
-    var monthlyRate = rate / 12;
-    var next = principle - (principle * monthlyRate)
-    return (principle - next)
-
-  }
 
 }
