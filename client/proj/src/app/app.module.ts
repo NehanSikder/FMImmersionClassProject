ad78085701d02efc21446c0dc80e738a22f7bcc2
Jason <jasonchen212@gmail.com>
2018-07-12 10:58:24 -0400

    front end stuff

--- client/proj/src/app/app.module.ts
+++ client/proj/src/app/app.module.ts
@@ -5,17 +5,24 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
-
+    FormsModule,
+		RouterModule.forRoot([
+      {
+        path: '',
+        component: DashboardComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
