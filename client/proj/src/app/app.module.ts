487512276c5d5b11d7af106ec6ca5d3462d02836
Tarik Hamdan <tkhamdani93@gmail.com>
2018-07-13 10:48:25 -0400

    New From

--- client/proj/src/app/app.module.ts
+++ client/proj/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { RealtorComponent } from './Realtor/realtor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RealtorComponent
   ],
   imports: [
     BrowserModule,
