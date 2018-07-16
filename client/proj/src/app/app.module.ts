07f67f3f82a7df7b98a713eec75201801924f745
Jason <jasonchen212@gmail.com>
2018-07-16 12:57:02 -0400

    UI Updates

--- client/proj/src/app/app.module.ts
+++ client/proj/src/app/app.module.ts
@@ -2,28 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RealtorComponent } from './Realtor/realtor.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    RealtorComponent
+    RealtorComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
 		RouterModule.forRoot([
-      {
-        path: '',
-        component: DashboardComponent
-      }
-    ])
+			{ 
+				path: '', 
+				component: LandingComponent 
+			},
+			{ 
+				path: 'landing', 
+				component: LandingComponent 
+      },
+      { 
+				path: 'home', 
+				component: HomeComponent 
+      },
+      { 
+				path: 'realtor', 
+				component: RealtorComponent 
+			}
+		])
   ],
   providers: [],
   bootstrap: [AppComponent]
