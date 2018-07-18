ab9b01dbe852cc8d647e9b1bf424916289266513
Eric <eyang24@terpmail.umd.edu>
2018-07-18 16:13:36 -0400

    ngFor added, kinda works but needs CSS improvements

--- client/proj/src/app/gallery/gallery.component.ts
+++ client/proj/src/app/gallery/gallery.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { APIService } from  '../api.service';
 
 @Component({
   selector: 'app-gallery',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  houses : Array<House>;
+  constructor(private http : HttpClient, private  apiService:  APIService) {
+    this.houses = new Array<House>();
   }
 
+  ngOnInit() {
+    this.apiService.getData().subscribe(data => {
+        console.log(typeof(data));
+        for (let house of data["houses"]) {
+          var newHouse = new House(
+            house.phone_number,
+            house.realtorEmail,
+            house.primary_key,
+            house.realtorFname,
+            house.realtorLname,
+            house.address,
+            house.price,
+            house.pictures
+          );
+          this.houses.push(newHouse);
+        }
+    }); 
+  }
+
+}
+
+export class House {
+  constructor(
+    public phone_number : string,
+    public realtorEmail : string,
+    public primary_key : number,
+    public realtorFname : string,
+    public realtorLname : string,
+    public address : string,
+    public price : number,
+    public pictures : Array<string>,
+  ) {
+
+  }
 }
