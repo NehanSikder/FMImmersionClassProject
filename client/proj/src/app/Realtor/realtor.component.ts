ca4ac70d448f128f7f337f68ff31688bff901f9d
Nehan <ans2ur@virginia.edu>
2018-07-19 12:25:24 -0400

    updated model

--- client/proj/src/app/Realtor/realtor.component.ts
+++ client/proj/src/app/Realtor/realtor.component.ts
@@ -1,140 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import * as AWS from 'aws-sdk';
-// import { HttpClient } from '@angular/common/http';
-// import { APIService } from  '../api.service';
-
-// @Component({
-//   selector: 'app-realtor',
-//   templateUrl: './realtor.component.html',
-//   styleUrls: ['./realtor.component.scss']
-// })
-
-// export class RealtorComponent implements OnInit {
-
-//   address: string;
-//   price: number;
-//   realtorFname: string;
-//   realtorLname: string;
-//   realtorEmail: string;
-//   phone_number: string;
-//   image: any;
-//   houseId: number;
-//   houseList: Array<object> = [];
-
-//   constructor(private http : HttpClient, private  apiService:  APIService){
-//   }
-
-//   ngOnInit() {
-//     this.apiService.getData().subscribe(data => {
-//         console.log(data);
-//     }); 
-//   }
-
-// /*
-// createContact(){
-
-// var  contact  = {
-//     account:  1,
-//     address:  "Home N 333 Apartment 300",
-//     createdBy:  1,
-//     description:  "This is the third contact",
-//     email:  "abbess@email.com",
-//     first_name:  "kaya",
-//     isActive: true,
-//     last_name: "Abbes",
-//     phone: "00121212101"
-// };
-// this.apiService.createContact(contact).subscribe((response) => {
-//     console.log(response);
-// });
-// };
-// }
-// }
-// */
-  
-//   submitPicPosting(picURL) {
-//     if (this.houseId == null) {
-//       // do nothing, shoudl create alert to tell user to first post a house!
-//     } else {
-//       var picture = {
-//         'houseFkey' : this.houseId,
-//         'picURL' : picURL
-//       }
-//       this.apiService.createPic(picture).subscribe((data) => {
-//         console.log(data);
-//       });
-      
-
-//     }  
-
-//   }
-//   submitHousePosting(){
-//     var house = {
-//         'address' : this.address,
-//         'price' : this.price,
-//         'realtorFname' : this.realtorFname,
-//         'realtorLname' : this.realtorLname,
-//         'realtorEmail' : this.realtorEmail,
-//         'phone_number' : this.phone_number,
-//     }
-//     this.apiService.createHouse(house).subscribe((data) => {
-//         console.log(data);
-//         this.houseId = data['houseID'];
-//     });  
-
-//   }
-
-
-//   fileEvent(fileInput: any) {
-//     const AWSService = AWS;
-//     const region = 'us-east-1';
-//     const bucketName = 'fmimmersionbuket';
-//     const IdentityPoolId = 'us-east-1:b6f9b2b1-792b-4cc3-98d5-4dc9788b30e8';
-//     const file = fileInput.target.files[0];
-//     console.log(file);
-//     var uuid;
-//       //Configures the AWS service and initial authorization
-//     AWSService.config.update({
-//       region: region,
-//       credentials: new AWSService.CognitoIdentityCredentials({
-//         IdentityPoolId: IdentityPoolId
-//       })
-//     });
-//   //adds the S3 service, make sure the api version and bucket are correct
-//     const s3 = new AWSService.S3({
-//       apiVersion: '2006-03-01',
-//       params: { Bucket: bucketName}
-//     });
-
-//     this.http.get('https://www.uuidgenerator.net/api/version1',{responseType: 'text'}).subscribe(data => {
-//         console.log(data);
-
-//         // creates the  unique name to be stores in the s3 bucket
-//         uuid = data.substring(0, data.length-2);
-//         this.image = uuid;
-//         s3.upload({ 
-//            Key: uuid, 
-//            Bucket: bucketName, 
-//            Body: file,
-//            ACL: 'public-read',
-//            ContentType: file.type
-//          }, function (err, data) {
-//            if (err) {
-//              console.log(err, 'there was an error uploading your file');
-//            } else {
-//              console.log('no error in S3 upload');
-//              this.submitPicPosting('https://s3.amazonaws.com/fmimmersionbuket/'+uuid);
-//            }
-//        });
-//     });
- 
-
-
-
-//   }
-
-
-// }
 import { Component, OnInit } from '@angular/core';
 import * as AWS from 'aws-sdk';
 import { HttpClient } from '@angular/common/http';
@@ -157,8 +20,6 @@ export class RealtorComponent implements OnInit {
   image: any;
   houseId: number;
   houseList: Array<object> = [];
-  latitude: number;
-  longitude: number;
   api_key: string = 'AIzaSyBEHLjxQrMH3BbMhtjYG89O-7IKx05uwPw'
 
   constructor(private http : HttpClient, private  apiService:  APIService){
@@ -223,11 +84,11 @@ this.apiService.createContact(contact).subscribe((response) => {
       address_parameter = address_parameter + "+" + address_split[i]
 
 
-    this.http.get<any>('https://maps.googleapis.com/maps/api/geocode/json?address=' + address_parameter + 'key=' + this.api_key)
-              .subscribe(function(response){
+    // this.http.get<any>('https://maps.googleapis.com/maps/api/geocode/json?address=' + address_parameter + 'key=' + this.api_key)
+    //           .subscribe(function(response){
 
-                  this.latitude = response.results[0].geometry.location.lat;
-                  this.longitude = response.results[0].geometry.location.lng;
+    //               this.latitude = response.results[0].geometry.location.lat;
+    //               this.longitude = response.results[0].geometry.location.lng;
 
                   var house = {
                       'address' : address,
@@ -235,9 +96,7 @@ this.apiService.createContact(contact).subscribe((response) => {
                       'realtorFname' : realtorFname,
                       'realtorLname' : realtorLname,
                       'realtorEmail' : realtorEmail,
-                      'phone_number' : phone_number,
-                      'latitude': this.latitude,
-                      'longitude': this.longitude
+                      'phone_number' : phone_number
                   }
 
                   console.log('House posted is: ', house)
@@ -246,7 +105,7 @@ this.apiService.createContact(contact).subscribe((response) => {
                       this.houseId = data['houseID'];
                   });
 
-              });;
+              // });;
   }
 
   fileEvent(fileInput: any) {
